196cd7f50a0d3e5374463dc2ba4239cc8f69896b
Herman Rödström <herman@rodstrom.se>
2012-05-16 20:00:56 +0200

    Streamlining creation of civilians
    Signed-off-by: Herman Rödström <herman@rodstrom.se>

--- Bombageddon/Bombageddon/Code/Entities/CivilianData.cs
+++ Bombageddon/Bombageddon/Code/Entities/CivilianData.cs
@@ -6,7 +6,7 @@ using Microsoft.Xna.Framework.Graphics;
 
 namespace Bombageddon.Code.Entities
 {
-    struct CivilianData
+    class CivilianData
     {
         public string civilianType;
 
@@ -16,9 +16,34 @@ namespace Bombageddon.Code.Entities
         public Texture2D randomDeathSheet1;
         public Texture2D randomDeathSheet2;
 
-        public int panicFramesCount;
-        public int splatDeathFramesCount;
-        public int deathFramesCount1;
-        public int deathFramesCount2;
+        public int panicFramesCount
+        {
+            get { return panicSheet.Width / panicSheet.Height; }
+        }
+        public int splatDeathFramesCount
+        {
+            get { return splatDeathSheet.Width / splatDeathSheet.Height; }
+        }
+        public int deathFramesCount1
+        {
+            get { return randomDeathSheet1.Width / randomDeathSheet1.Height; }
+        }
+        public int deathFramesCount2
+        {
+            get { return randomDeathSheet2.Width / randomDeathSheet2.Height; }
+        }
+
+        public CivilianData(Bombageddon game, String filename)
+        {
+            this.civilianType = filename;
+
+            filename = @"Graphics\Sheeples\" + filename + "\\" + filename;
+
+            panicSheet = game.Content.Load<Texture2D>(filename + "_panic");
+            collisionSheet = game.Content.Load<Texture2D>(filename + "_collision");
+            splatDeathSheet = game.Content.Load<Texture2D>(filename + "_splat");
+            randomDeathSheet1 = game.Content.Load<Texture2D>(filename + "_flatten");
+            randomDeathSheet2 = game.Content.Load<Texture2D>(filename + "_explosion");
+        }
     }
 }
--- Bombageddon/Bombageddon/Code/Entities/EntityManager.cs
+++ Bombageddon/Bombageddon/Code/Entities/EntityManager.cs
@@ -86,18 +86,8 @@ namespace Bombageddon.Code.Entities
 
         private void CreateCivilianData()
         {
-            CivilianData tmpCiv;
-            tmpCiv.civilianType = "1";
-            tmpCiv.panicSheet = game.Content.Load<Texture2D>(@"Graphics\Spritesheets\Man1sheet");
-            tmpCiv.collisionSheet = game.Content.Load<Texture2D>(@"Graphics\Collision\Man1_collision");
-            tmpCiv.panicFramesCount = 2;
-            tmpCiv.splatDeathSheet = game.Content.Load<Texture2D>(@"Graphics\Spritesheets\man1plattningsheet");
-            tmpCiv.splatDeathFramesCount = 5;
-            tmpCiv.randomDeathSheet1 = game.Content.Load<Texture2D>(@"Graphics\Spritesheets\man1explosionsheet");
-            tmpCiv.deathFramesCount1 = 5;
-            tmpCiv.randomDeathSheet2 = game.Content.Load<Texture2D>(@"Graphics\Spritesheets\man1kavlingsheet");
-            tmpCiv.deathFramesCount2 = 9;
-            civilianData.Add(tmpCiv);
+            CivilianData civilian = new CivilianData(game, "Man1");
+            civilianData.Add(civilian);
         }
 
         private LinkedListNode<Entity> findFirstOfType(String type)
@@ -205,76 +195,8 @@ namespace Bombageddon.Code.Entities
                 entity.Draw(gameTime);
             }
 
-            //player.Draw(gameTime);
-
             backgroundManager.Draw(gameTime, true);
-
-            //Texture2D t = new Texture2D(game.graphics.GraphicsDevice, 1, 1);
-            //t.SetData(new[] { Color.White }); 
-            //spriteBatch.Draw(t, new Rectangle((int)player.position.X - Bombageddon.WIDTH, Bombageddon.GROUND, Bombageddon.WIDTH * 4, 4), Color.Red); // Bottom
         }
-
-        //private void CollisionCheck()
-        //{
-        //    if (player.SourceRectangle.Bottom > Runner.HEIGHT)
-        //    {
-        //        player.lose = true;
-        //    }
-
-        //    player.falling = false;
-        //    foreach(Entity entity in entityList)
-        //    {
-        //        if (entity.GetType().Name == "Platform")
-        //        {
-        //            Platform tmpPlat = (Platform)entity;
-        //            if (!(player.Rectangle.Intersects(tmpPlat.HitRectangle)))
-        //            {
-        //                player.falling = true;
-        //            }
-        //            else if (player.Rectangle.Intersects(tmpPlat.HitRectangle))
-        //            {
-        //                SideCollided sides = GetSidesCollided(player.Rectangle, tmpPlat.HitRectangle);
-        //                if ((int)sides % 2 == (int)SideCollided.Top)
-        //                {
-        //                    player.position.Y = (tmpPlat.HitRectangle.Top - player.Rectangle.Height / 2) - 2;
-        //                    player.falling = false;
-        //                }
-        //                if (sides == SideCollided.Left)
-        //                {
-        //                    player.position.X = (tmpPlat.HitRectangle.Left - player.Rectangle.Width / 2) - 2;
-        //                    player.falling = true;
-        //                }
-        //                return;
-        //            }
-        //            if(tmpPlat.hitRect2Enabled)
-        //            {
-        //                if (!(player.Rectangle.Intersects(tmpPlat.HitRectangle2)))
-        //                {
-        //                    player.falling = true;
-        //                }
-        //                else if (player.Rectangle.Intersects(tmpPlat.HitRectangle2))
-        //                {
-        //                    SideCollided sides = GetSidesCollided(player.Rectangle, tmpPlat.HitRectangle2);
-        //                    if ((int)sides % 2 == (int)SideCollided.Top)
-        //                    {
-        //                        player.position.Y = (tmpPlat.HitRectangle2.Top - player.Rectangle.Height / 2) - 2;
-        //                        player.falling = false;
-        //                    }
-        //                    return;
-        //                }
-        //            }
-        //        }
-        //    }
-        //}
-
-        //public enum SideCollided
-        //{
-        //    None = 0x00,
-        //    Top = 0x01,
-        //    Bottom = 0x02,
-        //    Left = 0x04,
-        //    Right = 0x08,
-        //}
 
         private void CollisionCheck()
         {
